09dda506b256d5c125b10d5bfa830ff07e077404
ndenicolais <n.denicolais@arethusasrl.it>
2022-06-13 09:19:45 +0200

    First_Version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import QRCode from "qrcode.react";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  const [qrValue, setQrValue] = useState("qr_code_generated");
+  const handleOnChange = event => {
+    const { value } = event.target;
+    setQrValue(value);
+  };
+  const downloadQRCode = () => {
+    // Generate download with use canvas and stream
+    const canvas = document.getElementById("qr-gen");
+    const pngUrl = canvas
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    let downloadLink = document.createElement("a");
+    downloadLink.href = pngUrl;
+    downloadLink.download = `${qrValue}.png`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+  return (
+    <div className="App">
+      <h1>QR Code Generator</h1>
+      <input onChange={handleOnChange} placeholder="Insert here" />
+      <br />
+      <QRCode
+        id="qr-gen"
+        value={qrValue}
+        size={290}
+        level={"H"}
+        includeMargin={true}
+      />
+      <p>
+        <button type="button" onClick={downloadQRCode}>
+          Download QR Code
+        </button>
+      </p>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
