897af376db124097fc7909ebfaa62c8cad937a12
ndenicolais <n.denicolais@arethusasrl.it>
2022-06-16 12:17:36 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,58 +1,12 @@
-import React, { useState } from "react";
-import { createRoot } from 'react-dom/client';
-import QRCode from "qrcode.react";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Footer from './components/footer';
 
-function App() {
-  const [qrValue, setQrValue] = useState("qr_code_generated");
-  const handleOnChange = event => {
-    const { value } = event.target;
-    setQrValue(value);
-  };
-
-  const downloadQR = () => {
-    // Generate download with use canvas and stream
-    const canvas = document.getElementById("qr-code");
-    const pngQRCode = canvas
-      .toDataURL("image/png")
-      .replace("image/png", "image/octet-stream");
-    let linkQR = document.createElement("a");
-    linkQR.href = pngQRCode;
-    linkQR.download = `${qrValue}.png`;
-    document.body.appendChild(linkQR);
-    linkQR.click();
-    document.body.removeChild(linkQR);
-  };
-
-  return (
-    <div className="App">
-      <h1>QR Code Generator</h1>
-      <input
-      onChange={handleOnChange}
-      placeholder="Input value here"
-      />
-      <br /><br />
-      <QRCode
-        id="qr-code"
-        value={qrValue}
-        size={300}
-        bgColor="#FEB740"
-        fgColor="#EB3F43"
-        level={"H"}
-        includeMargin={true}
-        />
-      <p>
-        <button
-        type="button"
-        onClick={downloadQR}
-        >
-          Download QR Code
-        </button>
-      </p>
-    </div>
-  );
-}
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Footer />
+  </React.StrictMode>
+);
